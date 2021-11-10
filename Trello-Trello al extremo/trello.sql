14e55ebdea45d59e14a1e0f4b78c0996caa10618
nuhoquintero <hotbig001@gmail.com>
2021-11-09 23:16:41 -0300

    anadi en BD la tabla stock

--- Trello-Trello al extremo/trello.sql
+++ Trello-Trello al extremo/trello.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-11-2021 a las 20:33:38
--- Versión del servidor: 10.4.20-MariaDB
--- Versión de PHP: 8.0.9
+-- Tiempo de generación: 10-11-2021 a las 03:14:22
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,8 +29,46 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `login` (
   `correo` varchar(60) NOT NULL,
-  `contraseña` varchar(32) NOT NULL
+  `contraseña` varchar(32) NOT NULL,
+  `telefono` varchar(17) DEFAULT NULL,
+  `direccion` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `stock`
+--
+
+CREATE TABLE `stock` (
+  `id` int(11) NOT NULL,
+  `productoName` varchar(50) NOT NULL,
+  `stockUnidad` varchar(50) NOT NULL,
+  `tipoProducto` varchar(50) NOT NULL,
+  `precio` int(11) NOT NULL,
+  `fechaAlta` date DEFAULT NULL,
+  `fechaBaja` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `stock`
+--
+ALTER TABLE `stock`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
