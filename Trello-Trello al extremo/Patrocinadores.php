49f3b96d0ecf296492e217136cbbc4de7088222c
Ileana <1971ileana@gmail.com>
2021-11-15 16:06:12 -0300

    hice patrocinios

---
+++ Trello-Trello al extremo/Patrocinadores.php
@@ -0,0 +1,34 @@
+<?php
+include "nav.php";
+?>
+<html>
+<head>
+<link href="css/marcas.css" rel="stylesheet" type="text/css">
+    
+</head>
+<body>
+<div id="margin">
+<h1><b>Patrocinadores<b\></h2>
+</div>
+<br><br>
+<div class="marca">
+
+    <a href="https://tienda.fila.com.ar/" target="_blank"><img class="fila" src="images/fila.jpg" alt="logo fila"></a>
+
+    <a href="https://www.adidas.com.ar/" target="_blank"><img class="adidas" src="images/adidas.jpg" alt="logo adidas">  </a>
+
+    <a href="https://ar.puma.com/" target="_blank"><img class="puma" src="images/puma.jpg" alt="logo puma"></a>
+
+    <a href="https://www.amazon.com/-/es/ref=nav_logo" target="_blank"><img class="amazon" src="images/amazon.jpg" alt="logo amazon"></a>
+    <div class="conjunto">
+    <a href="https://www.underarmour.es/es-es/" target="_blank"><img class="under" src="images/under.png" alt=" logo under"></a>
+
+    <a href="https://www.reebok.com.ar/" target="_blank"><img class="reebok" src="images/reebok.jpg" alt="logo reebok"></a>
+
+    <a href="https://www.solodeportes.com.ar/" target="_blank"><img class="deportes" src="images/deportes.jpg" alt="logo solo deportes"></a>
+</div>
+
+
+</div>
+</body>
+</html>
