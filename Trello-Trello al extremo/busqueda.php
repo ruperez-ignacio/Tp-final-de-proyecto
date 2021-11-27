3560323cb788a6d1505fda10d0f97d392e4808f0
nuhoquintero <hotbig001@gmail.com>
2021-11-27 20:23:17 -0300

    añadi motor de busqueda

---
+++ Trello-Trello al extremo/busqueda.php
@@ -0,0 +1,32 @@
+<?php
+require_once "conexion.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="busqueda"><br>
+        <input type="submit" name="enviar" value="buscar">
+    </form>
+
+    <br><br><br>
+
+    <?php
+
+    if(isset($_POST['enviar'])){
+        $busqueda = $_POST['busqueda'];
+
+        $consulta = $con->query("SELECT * FROM trello WHERE desc_tipo_producto LIKE '%$busqueda%");
+
+        while($row=$consulta->fetch-array()){
+            echo $row['desc_tipo_producto'].'<br>';
+        }
+    }
+</body>
+</html>
